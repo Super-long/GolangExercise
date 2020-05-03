a3fa3a1774fa1fe5282d00df9074720ef08b9b58
Super-long <2339824768@qq.com>
2020-05-03 23:20:29 +0800

    daily

---
+++ leetcode/53.go
@@ -0,0 +1,18 @@
+package main
+
+func maxSubArray(nums []int) int {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] + nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
+
+func main() {
+
+}
